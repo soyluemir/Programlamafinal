d0565518e02ef301840fd4a633904fa82e1c7116
Emir Soylu <100766900+soyluemir@users.noreply.github.com>
2023-06-02 16:33:07 +0300

    Update Calisan.py
    zam hakkı

--- Calisan.py
+++ Calisan.py
@@ -15,3 +15,8 @@ class Calisan(Insan):
     def al_maas(self): return self.__maas
     def yap_maas(self,maas): self.__maas = maas
     
+    def zam_hakki(self):
+        if self.al_tecrube() < 24: return 0
+        elif self.al_tecrube() >= 24 and self.al_tecrube() <= 48 and self.al_maas() <= 15000: return self.al_maas() % self.al_tecrube()
+        elif self.al_tecrube() > 48 and self.al_maas() <= 25000: return (self.al_maas() % self.al_tecrube()) / 2
+        else: return 0
