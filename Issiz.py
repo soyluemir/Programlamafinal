64f01141329fba55dc58b3b61aa726f7320778d1
Emir Soylu <100766900+soyluemir@users.noreply.github.com>
2023-06-02 16:31:56 +0300

    Update Issiz.py
    statü bulma metodu

--- Issiz.py
+++ Issiz.py
@@ -3,3 +3,18 @@ class Issiz(Insan):
     def __init__(self, tcno, ad,soyad, yas, cinsiyet, uyruk, tecrube):
         super().__init__(tcno, ad, soyad, yas, cinsiyet, uyruk)
         self.__tecrube=tecrube
+    def statu_bul(self):
+        mavi, beyaz, yonetici = self.al_tecrube()["mavi"] * 0.2, self.al_tecrube()["beyaz"] * 0.35, self.al_tecrube()["yonetici"] * 0.45
+        try:
+            if yonetici >= beyaz and yonetici >= mavi:
+                self.yap_statu("Yönetici")
+            elif beyaz > yonetici and beyaz >= mavi:
+                self.yap_statu("Beyaz Yaka")
+            elif mavi > yonetici and mavi > beyaz:
+                self.yap_statu("Mavi Yaka")
+            else:
+                print("Hata oluştu")
+            return self.al_statu()
+        except:
+            print("Hata oluştu")
+    
