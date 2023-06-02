b996650a9cfd030910a5e45461fc797e3ca2e774
Emir Soylu <100766900+soyluemir@users.noreply.github.com>
2023-06-02 16:32:02 +0300

    Update Issiz.py

--- Issiz.py
+++ Issiz.py
@@ -3,6 +3,14 @@ class Issiz(Insan):
     def __init__(self, tcno, ad,soyad, yas, cinsiyet, uyruk, tecrube):
         super().__init__(tcno, ad, soyad, yas, cinsiyet, uyruk)
         self.__tecrube=tecrube
+        self.statu_bul()
+
+    def al_statu(self): return self.__statu
+    def yap_statu(self,statu): self.__statu=statu
+
+    def al_tecrube(self): return self.__tecrube
+    def yap_tecrube(self,tecrube): self.__tecrube = tecrube
+    
     def statu_bul(self):
         mavi, beyaz, yonetici = self.al_tecrube()["mavi"] * 0.2, self.al_tecrube()["beyaz"] * 0.35, self.al_tecrube()["yonetici"] * 0.45
         try:
@@ -18,3 +26,5 @@ class Issiz(Insan):
         except:
             print("Hata oluştu")
     
+    def __str__(self):
+        return "Kişi: "+super().__str__()+"\nStatü: "+self.statu_bul()
