06f9f53814253fcc621ebfa610b34e88d1e09ebe
Emir Soylu <100766900+soyluemir@users.noreply.github.com>
2023-06-04 11:14:58 +0300

    Update Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+from Insan import Insan
+from Issiz import Issiz
+from Calisan import Calisan
+from MaviYaka import MaviYaka
+from BeyazYaka import BeyazYaka
+import pandas as pd
+
+def main():
+if __name__ == "__main__":
+    main()
+
+    
